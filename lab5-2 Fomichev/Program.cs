73054b5a8b2f4c63b2239660db2b92e89e0f67be
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-11-22 22:54:26 +0200

    Добавьте файлы проекта.

---
+++ lab5-2 Fomichev/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите строку:");
+string str = Console.ReadLine();
+
+char[] charArray = str.ToCharArray();
+
+for (int i = 0; i < charArray.Length; i++)
+{
+    if (charArray[i] == 'я')
+    {
+        charArray[i] = ' ';
+    }
+    else
+    {
+        charArray[i] = (char)(charArray[i] + 1);
+    }
+}
+
+string newString = new string(charArray);
+
+Console.WriteLine("Измененная строка: " + newString);
